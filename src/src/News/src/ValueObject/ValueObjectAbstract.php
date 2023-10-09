a88faaddbac028be632457ee8bd251f250307fdb
bobubuntu <musgravehill@gmail.com>
2023-10-09 15:07:10 +0300

    Fast-Fail strategy. Add valueObject to validate user inputs.

---
+++ src/src/News/src/ValueObject/ValueObjectAbstract.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace News\ValueObject;
+
+use InvalidArgumentException;
+
+abstract class ValueObjectAbstract implements ValueObjectInterface
+{
+    //structural equality, compare
+    public function isEqualsTo(ValueObjectInterface $vo): bool
+    {
+        // new php $vo::class
+        if (get_class($this) !== get_class($vo)) {
+            throw new InvalidArgumentException('Objects of different classes');
+        }
+        
+        return true;
+        //It can NOT compare 2 object with plain Reflections::properties because some VO has objects\Enum as property
+        // $foo->name === $bar->name
+        // $foo->colorEnum === $bar->colorEnum no way!         
+    }      
+}

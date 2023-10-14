62ee6d6b0a02f4a9adaa629ffc1cd650b8e3e308
Bob-Eva-PC <musgravehill@gmail.com>
2023-10-14 20:38:04 +0300

    ORM  Doctrine migration

--- src/src/Oauth2/migrations/Version20231014173201.php
+++ src/src/Oauth2/migrations/Version20231014173201.php
@@ -25,12 +25,7 @@ final class Version20231014173201 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN oauth2_auth_code.userIdentifier IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE oauth2_refresh_token (identifier VARCHAR(80) NOT NULL, expiryDateTime TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, userIdentifier UUID NOT NULL, PRIMARY KEY(identifier))');
         $this->addSql('COMMENT ON COLUMN oauth2_refresh_token.expiryDateTime IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('COMMENT ON COLUMN oauth2_refresh_token.userIdentifier IS \'(DC2Type:uuid)\'');
-        $this->addSql('DROP INDEX idx_1dd399507b00651c');
-        $this->addSql('ALTER TABLE news ALTER text TYPE VARCHAR(255)');
-        $this->addSql('ALTER TABLE news ALTER created_date TYPE TIMESTAMP(0) WITHOUT TIME ZONE');
-        $this->addSql('ALTER TABLE news ALTER status SET NOT NULL');
-        $this->addSql('COMMENT ON COLUMN news.created_date IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN oauth2_refresh_token.userIdentifier IS \'(DC2Type:uuid)\'');              
     }
 
     public function down(Schema $schema): void
@@ -38,11 +33,6 @@ final class Version20231014173201 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE oauth2_auth_code');
-        $this->addSql('DROP TABLE oauth2_refresh_token');
-        $this->addSql('ALTER TABLE news ALTER text TYPE TEXT');
-        $this->addSql('ALTER TABLE news ALTER status DROP NOT NULL');
-        $this->addSql('ALTER TABLE news ALTER created_date TYPE TEXT');
-        $this->addSql('COMMENT ON COLUMN news.created_date IS NULL');
-        $this->addSql('CREATE INDEX idx_1dd399507b00651c ON news (status)');
+        $this->addSql('DROP TABLE oauth2_refresh_token');          
     }
 }
